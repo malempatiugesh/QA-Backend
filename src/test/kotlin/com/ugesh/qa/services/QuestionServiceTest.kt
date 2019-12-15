197a47bdfc928dc151db6d80535f1a2efa103077
Ugesh <malempati.u9@gmail.com>
2019-12-15 21:30:08 +0100

    write crud unit test for question service

--- src/test/kotlin/com/ugesh/qa/services/QuestionServiceTest.kt
+++ src/test/kotlin/com/ugesh/qa/services/QuestionServiceTest.kt
@@ -5,9 +5,7 @@ import com.ugesh.qa.dtos.payloads.questions.QuestionRequestPayload
 import com.ugesh.qa.exceptions.InvalidParameterException
 import com.ugesh.qa.models.Question
 import com.ugesh.qa.repositories.QuestionRepository
-import io.mockk.clearMocks
-import io.mockk.every
-import io.mockk.mockk
+import io.mockk.*
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.BeforeEach
@@ -18,10 +16,10 @@ import java.time.LocalDateTime
 import java.util.*
 
 class QuestionServiceTest {
-    //creating mocks once
     private val questionRepository: QuestionRepository = mockk()
     private val answerService: AnswerService = mockk()
     private val questionService = QuestionService(questionRepository = questionRepository, answerService = answerService)
+    private val questionId = UUID.randomUUID().toString().replace("-", "")
 
     @BeforeEach
     fun init() {
@@ -31,42 +29,144 @@ class QuestionServiceTest {
     @Nested
     inner class CreateQuestion {
         @Test
-        fun `creating a question should require question title`() {
-           assertThrows<InvalidParameterException> {
-                questionService.createQuestion(questionRequestPayload = QuestionRequestPayload(questionDescription = "xyz"))
-           }
+        fun `question should require question title`() {
+           assertThrows<InvalidParameterException> { questionService.createQuestion(
+                questionRequestPayload = QuestionRequestPayload(questionDescription = "xyz")
+           )}
         }
 
         @Test
-        fun `creating a question should require question description`() {
+        fun `question should require question description`() {
+            assertThrows<InvalidParameterException> { questionService.createQuestion(
+                questionRequestPayload = QuestionRequestPayload(questionTitle = "xyz")
+            )}
+        }
+
+        @Test
+        fun `question creation should work`() {
+            val question = questions()[0]
+            val createdQuestion = createQuestion()
+            assertNotNull(createdQuestion.questionId)
+            assertEquals(question.questionTitle, createdQuestion.questionTitle)
+            assertEquals(question.questionDescription, createdQuestion.questionDescription)
+            assertEquals(0, createdQuestion.totalAnswers)
+            assertEquals(0, createdQuestion.views)
+            assertEquals(0, createdQuestion.votes)
+
+            verify { questionRepository.save<Question>(any()) }
+        }
+    }
+
+    @Nested
+    inner class GetQuestions {
+        @Test
+        fun `getting all questions should work`() {
+            every {questionRepository.findAll()} returns questions()
+
+            val questions = questionService.getQuestions()
+            assertEquals(2, questions.total)
+            assertNotNull(questions.questions[0].questionId)
+            assertNotNull(questions.questions[1].questionId)
+
+            verify { questionRepository.findAll()}
+        }
+    }
+
+    @Nested
+    inner class GetQuestion {
+        @Test
+        fun `getting a single question should work`() {
+            every { questionRepository.findByQuestionId(questionId) } returns questions()[0]
+            every { questionRepository.save<Question>(any())} returns Question(
+                    id = 1,
+                    questionId = questionId,
+                    questionTitle = "what is postman?",
+                    questionDescription = "why and how postman is useful?",
+                    askedAt = LocalDateTime.now().toString(),
+                    views = 1
+            )
+            every { answerService.getAnswersByQuestionId(1) } returns listOf()
+
+            val question = questionService.getQuestion(questionId = questionId)
+            assertNotNull(question)
+            assertEquals("what is postman?", question.questionTitle)
+            assertEquals(1, question.views)
+            assertEquals(0, question.totalAnswers)
+
+            verifySequence {
+                questionRepository.findByQuestionId(question.questionId!!)
+                questionRepository.save<Question>(any())
+                answerService.getAnswersByQuestionId(1)
+            }
+        }
+    }
+
+    @Nested
+    inner class UpdateQuestion {
+        @Test
+        fun `update the question with empty title should not work`() {
+            val questionRequestPayload = QuestionRequestPayload(questionTitle = "")
+            every { questionRepository.findByQuestionId(questionId) } returns questions()[0]
+
             assertThrows<InvalidParameterException> {
-                questionService.createQuestion(questionRequestPayload = QuestionRequestPayload(questionTitle = "xyz"))
+                questionService.updateQuestion(questionId = questionId, questionRequestPayload = questionRequestPayload)
             }
         }
 
         @Test
-        fun `creating question should work`() {
-            val createdQuestion = createQuestion(title="title", description = "description")
-            assertNotNull(createdQuestion.questionId)
-            assertEquals("title", createdQuestion.questionTitle)
-            assertEquals("description", createdQuestion.questionDescription)
-            assertEquals(0, createdQuestion.totalAnswers)
-            assertEquals(0, createdQuestion.views)
-            assertEquals(0, createdQuestion.votes)
+        fun `update the question with empty description should not work`() {
+            val questionRequestPayload = QuestionRequestPayload(questionDescription = "")
+            every { questionRepository.findByQuestionId(questionId) } returns questions()[0]
+
+            assertThrows<InvalidParameterException> {
+                questionService.updateQuestion(questionId = questionId, questionRequestPayload = questionRequestPayload)
+            }
         }
     }
 
-    private fun createQuestion(title: String? = null, description: String? = null): QuestionDto {
-        every { questionRepository.save<Question>(any())} returns Question(
-                id = 1,
-                questionId = UUID.randomUUID().toString().replace("-", ""),
-                questionTitle = title!!,
-                questionDescription = description!!,
-                askedAt = LocalDateTime.now().toString()
-        )
+    @Nested
+    inner class DeleteQuestion {
+        @Test
+        fun `delete a question should work`() {
+            every { questionRepository.findByQuestionId(questionId) } returns questions()[0]
+            every { answerService.deleteAnswersByQuestionId(questionId = 1) } just Runs
+            every { questionRepository.delete(any()) } just Runs
+
+            questionService.deleteQuestion(questionId = questionId)
+
+            verifySequence {
+                questionRepository.findByQuestionId(questionId)
+                answerService.deleteAnswersByQuestionId(questionId = 1)
+                questionRepository.delete(any())
+            }
+        }
+    }
+
+    private fun createQuestion(): QuestionDto {
+        val question = questions()[0]
+        every { questionRepository.save<Question>(any())} returns questions()[0]
         return questionService.createQuestion(questionRequestPayload = QuestionRequestPayload(
-            questionTitle = title,
-            questionDescription = description
+            questionTitle = question.questionTitle,
+            questionDescription = question.questionDescription
         ))
+    }
+
+    private fun questions(): List<Question> {
+        return listOf(
+                Question(
+                id = 1,
+                questionId = questionId,
+                questionTitle = "what is postman?",
+                questionDescription = "why and how postman is useful?",
+                askedAt = LocalDateTime.now().toString()
+                ),
+                Question(
+                        id = 2,
+                        questionId = "2$questionId",
+                        questionTitle = "question two",
+                        questionDescription = "description two",
+                        askedAt = LocalDateTime.now().toString()
+                )
+        )
     }
 }
